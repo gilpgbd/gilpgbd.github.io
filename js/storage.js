39b3e132c620a7ebfe82a8e57356b3d75f14b13f
gilpgform <gilpgform@gmail.com>
2021-01-20 22:44:57 -0600

    Añade export

--- js/storage.js
+++ js/storage.js
@@ -1,12 +1,12 @@
 /** @param {string} nombre nombre con que se sube el archivo.
  * @param {FormDataEntryValue} archivo archivo a subir.
  * @returns {Promise<void>} */
-async function subeStorage(nombre, archivo) {
+export async function subeStorage(nombre, archivo) {
   await firebase.storage().ref(nombre).put(archivo);
 }
 /** @param {string} nombre nombre del archivo.
  * @returns {Promise<string>} */
-async function urlStorage(nombre) {
+export async function urlStorage(nombre) {
   try {
     return await firebase.storage().ref(nombre).getDownloadURL();
   } catch (e) {
@@ -16,6 +16,6 @@ async function urlStorage(nombre) {
 }
 /** @param {string} nombre nombre del archivo.
  * @returns {Promise<void>} */
-async function eliminaStorage(nombre) {
+export async function eliminaStorage(nombre) {
   return await firebase.storage().ref(nombre).delete();
 }
